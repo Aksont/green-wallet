25065735c33b22f7972120fa6404fa8092e2b56c
Aksont <aastanivuk@gmail.com>
2025-06-10 04:29:42 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,11 @@
-// src/main.ts
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { ExpressAdapter } from '@nestjs/platform-express';
-// import express from 'express';
-
-// const server = express();
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-//   app.setGlobalPrefix('api');
-//   app.enableCors();
-//   await app.init();
-// }
-
-// bootstrap();
-
-// export default server; // required by Vercel
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.PORT ?? 3001);
 }
-void bootstrap();
+bootstrap();
